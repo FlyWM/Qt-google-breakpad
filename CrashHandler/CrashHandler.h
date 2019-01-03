431d6902562ac0a51485f4f25c953443f2512fc8
gpf <gpf0728@163.com>
2019-01-03 15:37:17 +0800

    qt google breakpad

---
+++ CrashHandler/CrashHandler.h
@@ -0,0 +1,25 @@
+﻿#ifndef CRASHHANDLER_H
+#define CRASHHANDLER_H
+
+#include <QString>
+
+namespace Breakpad {
+    class CrashHandlerPrivate;
+    class CrashHandler
+    {
+    public:
+        static CrashHandler* instance();
+    void Init(const QString&  reportPath);
+
+        void setReportCrashesToSystem(bool report);
+        bool writeMinidump();
+
+    private:
+        CrashHandler();
+        ~CrashHandler();
+        Q_DISABLE_COPY(CrashHandler)
+        CrashHandlerPrivate* d;
+    };
+}
+
+#endif // CRASHHANDLER_H
